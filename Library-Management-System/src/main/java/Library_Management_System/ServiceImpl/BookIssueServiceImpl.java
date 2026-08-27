edf87d06f13480244f94736422b3f9f5ecfe4299
Arun M N <arun.m@simplify3x.com>
2026-08-27 13:21:27 +0530

    working on Bookissue Mng

--- Library-Management-System/src/main/java/Library_Management_System/ServiceImpl/BookIssueServiceImpl.java
+++ Library-Management-System/src/main/java/Library_Management_System/ServiceImpl/BookIssueServiceImpl.java
@@ -3,7 +3,9 @@ package Library_Management_System.ServiceImpl;
 import Library_Management_System.dto.Request.BookIssueRequest;
 import Library_Management_System.dto.Response;
 import Library_Management_System.dto.response.BookIssueResponse;
+import Library_Management_System.entity.BookIssue;
 import Library_Management_System.entity.BookManagement;
+import Library_Management_System.entity.Member;
 import Library_Management_System.repository.BookIssueRepository;
 import Library_Management_System.repository.BookManagementRepository;
 import Library_Management_System.repository.MemberRepository;
@@ -11,6 +13,7 @@ import Library_Management_System.service.BookIssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -24,6 +27,40 @@ public class BookIssueServiceImpl implements BookIssueService {
 
     @Override
     public Response issueBook(BookIssueRequest request) {
+
+        BookManagement book = bookManagementRepository.findById(request.getBookId())
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        Member member = memberRepository.findById(request.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        if (book.getQuantity() <= 0) {
+            throw new RuntimeException("Book quantity less than 0");
+        }
+
+        BookIssue bookIssue = new BookIssue();
+
+        bookIssue.setBook(book);
+        bookIssue.setMember(member);
+        bookIssue.setIssueDate(new Date());
+        bookIssue.setDueDate(request.getDueDate());
+        bookIssue.setStatus("Issued");
+
+        book.setQuantity(book.getQuantity() - 1);
+
+        bookIssueRepository.save(bookIssue);
+
+        BookIssue savedIssue = bookIssueRepository.save(bookIssue);
+
+        BookIssueResponse response = new BookIssueResponse();
+
+        response.setIssueId(savedIssue.getId());
+        response.setBookId(savedIssue.getBook().getId());
+        response.setMemberId(savedIssue.getMember().getId());
+        response.setIssueDate(savedIssue.getIssueDate());
+        response.setDueDate(savedIssue.getDueDate());
+        response.setStatus(savedIssue.getStatus());
+
         return null;
     }
 
